c4c8b68a5559123342ff6f17995144cf51688592
Chidugam Njobuenwu <22CNjobuenwu@heckgrammar.co.uk>
2025-01-08 12:20:08 +0000

    done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Work
+﻿using System;
+
+namespace Work
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            int allowed_height = 140;
+            int allowed_age = 11;
+
+            Console.WriteLine("How old are you");
+            int age = (Console.ReadLine());
         }
     }
 }
