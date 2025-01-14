47b2a2cefe75a814ba141cc769b0e5ad76c5a618
Chidugam Njobuenwu <22CNjobuenwu@heckgrammar.co.uk>
2025-01-14 12:06:27 +0000

    More chanegs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Work
 {
@@ -6,11 +7,45 @@ namespace Work
     {
         static void Main(string[] args)
         {
-            int allowed_height = 140;
-            int allowed_age = 11;
+            //int allowed_height = 140;
+            //int allowed_age = 11;
 
-            Console.WriteLine("How old are you");
-            int age = (Console.ReadLine());
+            //Console.WriteLine("How old are you");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine("How tall are you?");
+            //int height = int.Parse(Console.ReadLine());
+
+            //if (age < allowed_age || height < allowed_height)
+            //{
+            //    Console.WriteLine("You cannot go on this ride");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are allowed on this ride, have fun!");
+            //}
+
+            //Console.WriteLine("What is the total cost of your items???");
+            //double price = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Do you have a membership? Press Y or N");
+            //string membership = Console.ReadLine();
+            //if (membership == "Y") {
+            //    Console.WriteLine("Your price is reduced by 20%");
+            //    price = price * 0.8;
+            //    price = Math.Round(price, 2);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your price is not reduced");
+            //}
+            //Console.WriteLine("Your total price is " + price);
+
+            Console.WriteLine("Do you want me to calculate a "
+
+
+
+
+
         }
     }
 }
